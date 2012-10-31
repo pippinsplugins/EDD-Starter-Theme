e85b043d9936aba4a142c0184957daffe46b89d1
Will <willembisschoff@gmail.com>
2012-10-31 09:26:14 +0000

    removed <?php?> tags which were giving a Notice: Use of undefined constant php

--- searchform.php
+++ searchform.php
@@ -1,6 +1,4 @@
-<?php?>
 <form role="search" method="get" id="searchform" action="<?php bloginfo('url'); ?>">
 	<input type="text" name="s" id="searchinput" placeholder="Search . . ."/>
 	<input type="submit" name="ssubmit" id="searchsubmit" value="Submit"/>
 </form>
-<?php?>
