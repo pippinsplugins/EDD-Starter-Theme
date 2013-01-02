28ccebd4785b5c1c29f93b699bab55cc6af955e0
Andrew <andrewmunro@me.com>
2013-01-03 11:03:29 +1300

    Fixed pagination on front-page.php
    - Removed use of WP_Query and altered main loop using the pre_get_posts action hook.
    - Used $query->set to define number of downloads to show and set post type.

--- front-page.php
+++ front-page.php
@@ -2,19 +2,11 @@
 
 	<div id="main-content" class="row store-template">
 		<div class="content clearfix">
-			<?php
-			$current_page = get_query_var('page');
-			$per_page = get_option('posts_per_page');
-			$offset = $current_page > 0 ? $per_page * ($current_page-1) : 0;
-			$product_args = array(
-				'post_type' => 'download',
-				'posts_per_page' => $per_page,
-				'offset' => $offset
-			);
-			$products = new WP_Query($product_args);
-			?>
-			<?php if ($products->have_posts()) : $i = 1; ?>
-				<?php while ($products->have_posts()) : $products->the_post(); ?>
+			
+			<?php if ( have_posts() ) : $i = 1; ?>
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
 					<div class="threecol product<?php if($i % 4 == 0) { echo ' last'; } ?>">
 						<a href="<?php the_permalink(); ?>">
 							<h2 class="title"><?php the_title(); ?></h2>
@@ -49,15 +41,18 @@
 				<?php endwhile; ?>
 				
 				<div class="pagination">
-					<?php 					
-						$big = 999999999; // need an unlikely intege					
-						echo paginate_links( array(
+
+					<?php 
+						$big = 999999999; // need an unlikely integer
+						echo paginate_links(array(
 							'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
 							'format' => '?paged=%#%',
-							'current' => max( 1, $current_page ),
-							'total' => $products->max_num_pages
-						) );
+							'current' => max( 1, get_query_var('paged') ),
+							'total' => $wp_query->max_num_pages
+						));
+
 					?>
+
 				</div>
 			<?php else : ?>
 		
